63d6f13ca6bb1c94f3cdd484d199adcef6539f82
Artur <kashtaevartur@gmail.com>
2022-11-26 13:45:50 +0200

    Изменил диапазон до 1000

---
+++ main.py
@@ -0,0 +1,15 @@
+from random import randint
+
+
+ran= randint(1, 1000)
+print("Отгадайте число от 1 до 1000")
+while (True):
+    inp = int(input("Введите число от 1 до 1000__"))
+    if inp <ran :
+      print("Ваше число меньше загаданного")
+    elif inp>ran:
+        print("Ваше число больше загаданного")
+    else:
+        print("Вы угадали число", ran)
+        break
+    
